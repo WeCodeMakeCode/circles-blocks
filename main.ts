180407596bbc4aba7c99cf80bf882d17d2a01ced
WeCodeMakeCode <52891037+WeCodeMakeCode@users.noreply.github.com>
2020-05-18 11:19:08 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,34 +2,38 @@ namespace SpriteKind {
     export const Ball = SpriteKind.create()
     export const Circle = SpriteKind.create()
 }
+sprites.onOverlap(SpriteKind.Circle, SpriteKind.Circle, function (sprite, otherSprite) {
+    if (sprite.width < otherSprite.width) {
+        otherSprite.follow(sprite)
+    } else {
+        sprite.follow(otherSprite)
+    }
+})
+function get_color () {
+    color_number = (color_number + 1) % 12
+}
+function add_circle () {
+    Radius = Math.randomRange(20, 60)
+    info.setScore(color_number)
+    get_color()
+    make_circle(Radius, color_number)
+    Circle_sprite.setFlag(SpriteFlag.BounceOnWall, true)
+    Circle_sprite.setVelocity(Math.randomRange(-100, 100), Math.randomRange(-100, 100))
+}
 function make_circle (Radius: number, Color: number) {
     Wh = 2 * Radius
-    Circle_sprite = sprites.create(image.create(Wh, Wh), SpriteKind.Circle)
+    circle_image = image.create(Wh, Wh)
+    Circle_sprite = sprites.create(circle_image, SpriteKind.Circle)
     Center_x = Circle_sprite.width / 2
     Center_y = Center_x
-    N_points = 3 * Radius
-    Degrees_per_point = Math.round(360 / N_points)
-    for (let index = 0; index <= N_points - 1; index++) {
+    N_points = 4 * Radius
+    Degrees_per_point = Math.ceil(360 / N_points)
+    for (let index = 0; index <= N_points; index++) {
         Degrees = index * Degrees_per_point
         degrees_to_xy(Degrees, Radius, Center_x, Center_y)
         Circle_sprite.image.setPixel(Return_x, Return_y, Color)
     }
 }
-function add_circle () {
-    Radius = Math.randomRange(20, 80)
-    make_circle(Radians, 3)
-    Circle_sprite.setFlag(SpriteFlag.BounceOnWall, true)
-    Circle_sprite.setVelocity(Math.randomRange(-100, 100), Math.randomRange(-100, 100))
-}
-sprites.onOverlap(SpriteKind.Ball, SpriteKind.Ball, function (sprite, otherSprite) {
-    if (sprite.width < otherSprite.width) {
-        sprite.setVelocity(0 - otherSprite.vx, 0 - otherSprite.vy)
-        sprite.destroy()
-    } else {
-        otherSprite.setVelocity(0 - sprite.vx, 0 - sprite.vy)
-        otherSprite.destroy()
-    }
-})
 controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
     add_circle()
 })
@@ -46,12 +50,15 @@ let Return_y = 0
 let Return_x = 0
 let Degrees = 0
 let Degrees_per_point = 0
-let Radius = 0
 let N_points = 0
 let Center_y = 0
 let Center_x = 0
-let Circle_sprite: Sprite = null
+let circle_image: Image = null
 let Wh = 0
+let Circle_sprite: Sprite = null
+let Radius = 0
+let color_number = 0
 let Pi = 0
 Pi = parseFloat("3.14159265359")
+color_number = 0
 add_circle()
