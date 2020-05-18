2a1416163326932f884636a94febd4230e4a1be5
WeCodeMakeCode <52891037+WeCodeMakeCode@users.noreply.github.com>
2020-05-18 10:39:24 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,25 @@
 namespace SpriteKind {
     export const Ball = SpriteKind.create()
+    export const Circle = SpriteKind.create()
 }
-function add_ball () {
-    R = Math.randomRange(20, 40)
-    if (R % 2 == 0) {
-        R += 1
+function make_circle (Radius: number, Color: number) {
+    Wh = 2 * Radius
+    Circle_sprite = sprites.create(image.create(Wh, Wh), SpriteKind.Circle)
+    Center_x = Circle_sprite.width / 2
+    Center_y = Center_x
+    N_points = 3 * Radius
+    Degrees_per_point = Math.round(360 / N_points)
+    for (let index = 0; index <= N_points - 1; index++) {
+        Degrees = index * Degrees_per_point
+        degrees_to_xy(Degrees, Radius, Center_x, Center_y)
+        Circle_sprite.image.setPixel(Return_x, Return_y, Color)
     }
-    Circle_sprite = sprites.create(image.create(R, R), SpriteKind.Ball)
-    draw_circle(Circle_sprite, 3.6 * R, Math.randomRange(2, 10))
+}
+function add_circle () {
+    Radius = Math.randomRange(20, 80)
+    make_circle(Radians, 3)
     Circle_sprite.setFlag(SpriteFlag.BounceOnWall, true)
     Circle_sprite.setVelocity(Math.randomRange(-100, 100), Math.randomRange(-100, 100))
-}
-function draw_circle (Square_sprite: Sprite, N_points: number, This_color: number) {
-    Degrees_per_point = Math.round(360 / N_points)
-    Center_x = Square_sprite.width / 2
-    Center_y = Square_sprite.width / 2
-    for (let index = 0; index <= 359; index++) {
-        Degrees = index + 1
-        if (Degrees % Degrees_per_point == 0) {
-            degrees_to_xy(Degrees, Center_x - 1, Center_x, Center_y)
-            Square_sprite.image.setPixel(Return_x, Return_y, This_color)
-        }
-    }
 }
 sprites.onOverlap(SpriteKind.Ball, SpriteKind.Ball, function (sprite, otherSprite) {
     if (sprite.width < otherSprite.width) {
@@ -33,10 +31,10 @@ sprites.onOverlap(SpriteKind.Ball, SpriteKind.Ball, function (sprite, otherSprit
     }
 })
 controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
-    add_ball()
+    add_circle()
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    add_ball()
+    add_circle()
 })
 function degrees_to_xy (Degrees: number, Radius: number, Center_x: number, Center_y: number) {
     Radians = Degrees * Pi / 180
@@ -47,11 +45,13 @@ let Radians = 0
 let Return_y = 0
 let Return_x = 0
 let Degrees = 0
+let Degrees_per_point = 0
+let Radius = 0
+let N_points = 0
 let Center_y = 0
 let Center_x = 0
-let Degrees_per_point = 0
 let Circle_sprite: Sprite = null
-let R = 0
+let Wh = 0
 let Pi = 0
 Pi = parseFloat("3.14159265359")
-add_ball()
+add_circle()
